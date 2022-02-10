d31900f1889e1956e0a7b1bc8f47305fc7305db4
themarkr <markrodriguezaa@gmail.com>
2022-02-10 15:16:23 -0500

    input validation working, more cases needed

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 window.addEventListener('DOMContentLoaded', () => {
 
     const board = document.querySelector('#board')
-    const rows = board.childNodes;
-    let numOfRows = rows.length;
+    let numOfRows = board.children.length;
     let attempts = 0;
+    let gameOver = false;
 
     const row1Tiles = document.getElementById('row1').children;
     // const wrong = document.getElementById('')
@@ -11,28 +11,43 @@ window.addEventListener('DOMContentLoaded', () => {
     const row3Tiles = document.getElementById('row3').children;
     const row4Tiles = document.getElementById('row4').children;
     const row5Tiles = document.getElementById('row5').children;
-    const submitForm = document.querySelector('form');
+    const submitButton = document.getElementById('submit-button');
+    const errorMessage = document.getElementById('error-message');
 
     let answer = pickWord();
     console.log(answer);
 
     function changeColor(tile, i, guessWord) {
         let tileText = tile.innerText;
-        console.log(tile, i, guessWord, tileText);
         if (tileText === answer[i]) {
             tile.style.backgroundColor = "#538d4e";
+            tile.style.borderColor = "#538d4e"
+                // changeKeyboardColor(tileText, )
         } else if (answer.includes(tileText)) {
             tile.style.backgroundColor = "#b59f3b";
+            tile.style.borderColor = "#b59f3b"
         } else {
             tile.style.backgroundColor = "#3a3a3c";
         }
     }
 
+    // function changeKeyboardColor(tile, i, guessWord) {
+    //     let tileText = tile.innerText;
+    //     console.log(tile, i, guessWord, tileText);
+    //     if (tileText === answer[i]) {
+    //         tile.style.backgroundColor = "#538d4e";
+    //     } else if (answer.includes(tileText)) {
+    //         tile.style.backgroundColor = "#b59f3b";
+    //     } else {
+    //         tile.style.backgroundColor = "#3a3a3c";
+    //     }
+    // }
+
     function updateTiles(totalAttempts) {
         let guess = document.getElementById('guess-input').value
         switch (totalAttempts) {
             case 1:
-                console.log(row1Tiles);
+                // console.log(row1Tiles);
                 for (let i = 0; i < row1Tiles.length; i++) {
                     row1Tiles[i].innerText = guess[i];
                     changeColor(row1Tiles[i], i, guess);
@@ -62,17 +77,40 @@ window.addEventListener('DOMContentLoaded', () => {
                     changeColor(row5Tiles[i], i, guess);
                 }
                 break;
-                // left off at creating a default case 
-                // default:
+                //to do 
+                // input validation
+                // game over check
+                // disable button on some checks
+                // new game button that will
+                //      clear the board
+                //      generate a new random word for the game
+                //      reset attempts to 0
+
         }
     }
-
-    function onSubmit(event) {
-        event.preventDefault();
-        let guess = document.getElementById('guess-input').value
-        attempts++;
-        console.log(answer, guess, attempts);
-        updateTiles(attempts);
+    // console.log(numOfRows);
+    if (attempts >= numOfRows) {
+        submitForm.disabled = true;
+        c
     }
-    submitForm.addEventListener('submit', onSubmit);
-});
+
+    function onSubmit(event) { // function for when we hit submit
+        event.preventDefault(); // prevent the page from reloading
+        errorMessage.innerText = ""
+            //get guess from input field
+        let guess = document.getElementById('guess-input').value
+            // input validation 
+            // check if guess is correct length and an english word
+        if (guess.length === answer.length && filteredWordBank.includes(guess)) {
+            // if it is, update attempts by 1
+            // call update tiles function
+            // reset input field to be blank
+            attempts++;
+            updateTiles(attempts);
+        } else if (guess.length === answer.length && !filteredWordBank.includes(guess)) {
+            errorMessage.innerText = "Thats not a word!";
+        }
+        document.getElementById('guess-input').value = "";
+    }
+    submitButton.addEventListener('click', onSubmit);
+})
