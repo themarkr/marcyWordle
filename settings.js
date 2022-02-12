7d80fe14062270d349138b5e7aec8dff009b3ca9
Jayleaf1125 <jasonpadilla104@gmail.com>
2022-02-11 20:16:32 -0500

    Adding & Deleting Comments

--- settings.js
+++ settings.js
@@ -4,8 +4,23 @@
     - Set the class equals to "row"
     - Set the id equals to `$row{currentSteps}`
     - Create five div's that have their class set to "tile" and append each to the parent DIV
-    - Append the recently create parent DIV to the "board" parent div    
- */
+    - Append the recently create parent DIV to the "board" parent div   
+
+Adding/Deleting Rows =>
+    - Create a temp variable to hold in the pevious diffclty setting
+    - Then compare the current diffculty setting with the value of the temp variable
+        - If the temp variable is greater than the current va;ue of the diffculyt setting, delete the row that has the value of temp
+        - If the temp variable is less than the current value of the difcculty setting, add the row with the value of current
+Increasing/Decreasing Word Length =>
+    - Get the childrens of said row (5 being the default)
+    - If the slider is increased, loop till you reach the end, add tiles and so on
+    
+oninput => Track the diffculty
+onchange => Track the change in diffculty
+remove => removes the element from the tree it belongs to 
+
+*/
+ 
 
 /* Targets the element, Diffculty */
 const diffcultySetting = document.querySelector('#diffculty')
@@ -16,50 +31,23 @@ console.log(defaultSetting)
 
 /* This allows the slider of the diffculty setting to be tracked */
 diffcultySetting.addEventListener('input', rowManpulation);
-// diffcultySetting.addEventListener('input', deletingRows);
 
 let temp = defaultSetting;
 
 function rowManpulation(e) {
     const diffcultyLevel = +e.target.value;
-    console.log(diffcultyLevel)
     if (temp < diffcultyLevel) {
         rowMaker(diffcultyLevel);
         temp++;
-        console.log(`adding ${temp}`)
         return
     }
-    
     if (temp > diffcultyLevel) {
-        // debugger;
         rowRemover(diffcultyLevel + 1);
         temp--;
-        console.log(`removing ${temp}`)
         return
     }
-    
 }
 
-
-/*
-Adding/Deleting Rows =>
-    - Create a temp variable to hold in the pevious diffclty setting
-    - Then compare the current diffculty setting with the value of the temp variable
-        - If the temp variable is greater than the current va;ue of the diffculyt setting, delete the row that has the value of temp
-        - If the temp variable is less than the current value of the difcculty setting, add the row with the value of current
-Increasing/Decreasing Word Length =>
-    - Get the childrens of said row (5 being the default)
-    - If the slider is increased, loop till you reach the end, add tiles and so on
-    -
-    -
-    -
-
-*/
-
-
-// oninput => Track the diffculty
-// onchange => Track the change in diffculty
-// remove => removes the element from the tree it belongs to
 
 /* Creates the new rows */
 const board = document.querySelector('#board')
@@ -92,3 +80,4 @@ function rowRemover(level) {
     const ele = document.querySelector(`#row${level}`);
     ele.remove();
 }
+
